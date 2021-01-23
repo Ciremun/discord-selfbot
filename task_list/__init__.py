d579c40892bc4b972c0ed260030bd2325c74ec3f
Ciremun <Ciremun@yandex.ru>
2021-01-23 15:55:32 +0300

    move main

--- task_list/__init__.py
+++ task_list/__init__.py
@@ -1,5 +1,18 @@
 from flask import Flask
 
+def main():
+    import os
+    from os.path import join, dirname
+    from dotenv import load_dotenv
+
+    import src.config
+    import src.commands
+    from src.client import client
+
+    load_dotenv(join(dirname(__file__), '.env'))
+
+    client.run(os.environ.get('DISCORD_TOKEN'), bot=src.config.bot)
+
 def create_app():
     app = Flask(__name__)
 
