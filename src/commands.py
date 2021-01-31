95646e4a48f2f7fe4fd426fa9b01a3ac97557422
Ciremun <Ciremun@yandex.ru>
2021-01-31 03:35:03 +0300

    unicode emojis, refactor

--- src/commands.py
+++ src/commands.py
@@ -1,12 +1,17 @@
 import re
+import io
+import os
 
 import discord
+import requests
+import cairosvg
 
-from .utils import send_error
+from .utils import send_error, unicode_emojis
 from .client import client
 
 discord_emoji_re = re.compile(r'<a?:(\w+|\d+):(\d{18})>')
 size_re = re.compile(r'size=(\d+)')
+unusual_char_re = re.compile(r'[^\w\s,]')
 commands = {}
 
 def command(*, name: str):
@@ -56,13 +61,19 @@ async def emoji_command(message: discord.Message):
     message_split = message.content.split(' ')
     emoji_name = message_split[1]
     async def send_emoji_link(emoji: discord.Emoji):
-        end = 'gif' if emoji.animated else 'png'
         size = '' if len(message_split) < 3 else f'?size={message_split[2]}'
-        await message.channel.send(f'https://cdn.discordapp.com/emojis/{emoji.id}.{end}{size}')
+        await message.channel.send(f'{emoji.url}{size}')
     if emoji_id := re.search(discord_emoji_re, emoji_name):
         if emoji := client.get_emoji(int(emoji_id.group(2))):
             await send_emoji_link(emoji)
-    elif emoji := discord.utils.get([e for g in client.guilds for e in g.emojis], name=emoji_name):
+    elif emoji := discord.utils.get(client.emojis, name=emoji_name):
         await send_emoji_link(emoji)
     else:
+        if re.match(unusual_char_re, emoji_name):
+            for emoji in unicode_emojis:
+                if emoji['emoji'] == emoji_name:
+                    svg = requests.get(emoji['svg']).content
+                    png = cairosvg.svg2png(svg)
+                    await message.channel.send(file=discord.File(fp=io.BytesIO(png), filename='image.png'))
+                    return
         await send_error(f'emoji {emoji_name} not found', message, delay=3)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
+import json
 from typing import Optional
 
 import discord
+
+unicode_emojis = json.load(open('emojis.json'))
 
 async def send_error(error_message: str, message: discord.Message, *, delay: Optional[float] = None):
     message = await message.channel.send(error_message)
