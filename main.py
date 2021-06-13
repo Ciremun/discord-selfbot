edd119f146e7d544b8cb2750dd9a91d5fee87b86
Ciremun <Ciremun@yandex.ru>
2021-06-13 19:22:03 +0300

    make it run forever

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import os
-import asyncio
-
-from dotenv import load_dotenv
-
-from src.client import Client
-
-
 async def main():
+    from dotenv import load_dotenv
     load_dotenv()
+    from src.client import Client
     clients = []
+    import os
     for token in os.environ.get('DISCORD_SELFBOT_TOKENS').split(';'):
         client = Client()
         client.loop.create_task(client.start(token, bot=False))
         clients.append(client)
-    await asyncio.sleep(6969)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import asyncio
+    loop = asyncio.get_event_loop()
+    try:
+        loop.create_task(main())
+        loop.run_forever()
+    finally:
+        loop.close()
